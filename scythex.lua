b998dcc8c7a70a1f6ed0d2de6ff2ec26efcb72c8
n1x <pnipon74@gmail.com>
2025-07-01 18:10:29 +0300

    Update scythex.lua

--- scythex.lua
+++ scythex.lua
@@ -9,7 +9,7 @@ local Global = {
                 RandomiseTargetPlayer = false,
                 Teleporter = {
                     Enabled = true,
-                    JobId = "87a7d9be-f7ac-4fe3-bf46-ac103b3e5382" -- setclipboard(game.JobId)
+                    JobId = "87a7d9be-f7ac-4fe3-bf46-ac103b3e5382", -- setclipboard(game.JobId)
                     Script = "https://raw.githubusercontent.com/scytheXlol/Grow-A-Garden-OP-Methods/refs/heads/main/scythex.lua"
                 },
                 TargetItems = {
